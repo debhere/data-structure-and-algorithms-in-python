7ee1a51b3cf0fb35e74920e576b7e9905d479177
debhere <debmalya.mondal@outlook.com>
2025-08-16 23:47:05 +0530

    insert and getLength method inclusion

--- linked_list/CircularSinglyLinkedList.py
+++ linked_list/CircularSinglyLinkedList.py
@@ -21,6 +21,19 @@ class CircularSinglyLinkedList:
             current = current.next
         return result
 
+    def getLength(self):
+        length = 0
+        current = self.head
+        if self.head == self.tail:
+            length += 1
+        else:
+            while current:
+                length += 1
+                if current.next == self.head:
+                    break
+                current = current.next
+        return length
+
     def append(self, value):
         current = self.head
         node = Node(value)
@@ -46,8 +59,27 @@ class CircularSinglyLinkedList:
             self.tail.next = node
             self.head = node
 
-    def insert(self, index, value):
-        pass
+    def insert(self, index, value) -> bool:
+        isInserted = False
+        if index == 0:
+            self.prepend(value)
+            isInserted = True
+        elif index == self.getLength() - 1:
+            self.append(value)
+            isInserted = True
+        elif index >= self.getLength():
+            isInserted = False
+        elif index < self.getLength() - 1:
+            node = Node(value)
+            current = self.head
+            loc = 0
+            while loc < index - 1:
+                current = current.next
+                loc += 1
+            node.next = current.next
+            current.next = node
+            isInserted = True
+        return isInserted
 
     def isEmpty(self):
         return self.head is None
@@ -57,13 +89,19 @@ if __name__ == "__main__":
     csll = CircularSinglyLinkedList()
     print("--------Checking if Linked List is Empty----------")
     print(csll.isEmpty())
-    print("--------Appending data in Linked List----------")
+    print("--------Appending data into Linked List----------")
     csll.append(10)
     csll.append(20)
     print(csll)
     print(csll.head.value, csll.tail.value)
     print(csll.isEmpty())
-    print("--------Prepending data in Linked List----------")
+    print("--------Prepending data into Linked List----------")
     csll.prepend(5)
     csll.prepend(-10)
+    print(csll)
+    print("--------Length of Linked List----------")
+    print(csll.getLength())
+    print("--------Inserting data into Linked List----------")
+    print(csll.insert(2, 100))
+    print(csll.insert(4, 50))
     print(csll)
