96eb8f2137a143859d9b22f087386a4d93673ba9
debhere <debmalya.mondal@outlook.com>
2025-10-23 20:35:32 +0530

    fibonacci update

--- dynamic_programming/fibonacci.py
+++ dynamic_programming/fibonacci.py
@@ -1,7 +1,5 @@
-from typing import Any
-
-
-def fibonacci_sum(n: int, memo=None) -> int:
+# This is a memoization example
+def fibonacci_memo(n: int, memo=None) -> int:
     if memo is None:
         memo: dict = {}
     if n == 0:
@@ -10,11 +8,29 @@ def fibonacci_sum(n: int, memo=None) -> int:
         return 1
     if n in memo:
         return memo[n]
-    memo[n] = fibonacci_sum(n - 1, memo) + fibonacci_sum(n - 2, memo)
+    memo[n] = fibonacci_memo(n - 1, memo) + fibonacci_memo(n - 2, memo)
     return memo[n]
 
 
+def fibonacci_tabulation(n):
+    if n <= 1:
+        return n
+
+    dlist = [0] * (n + 1)
+    dlist[0] = 0
+    dlist[1] = 1
+
+    for i in range(2, n+1):
+        dlist[i] = dlist[i-2] + dlist[i-1]
+
+    return dlist[n]
+
+
 if __name__ == "__main__":
-    print(fibonacci_sum(8))
-    print(fibonacci_sum(10))
-    print(fibonacci_sum(50))
+    print(fibonacci_memo(8))
+    print(fibonacci_memo(10))
+    print(fibonacci_memo(50))
+
+    print(fibonacci_tabulation(8))
+    print(fibonacci_tabulation(10))
+    print(fibonacci_tabulation(50))
