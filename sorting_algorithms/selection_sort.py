6cf0b339127a33f787db91d8f6aef4b785838d91
debhere <debmalya.mondal@outlook.com>
2025-10-26 14:36:15 +0530

    selection sort

--- sorting_algorithms/selection_sort.py
+++ sorting_algorithms/selection_sort.py
@@ -1,7 +1,7 @@
 def selectionSort(nums: list[int]):
     for i in range(len(nums)):
         min_index = i
-        for j in range(1, len(nums)):
+        for j in range(i, len(nums)):
             if nums[min_index] > nums[j]:
                 min_index = j
         nums[i], nums[min_index] = nums[min_index], nums[i]
