4b996f9476a19d4ca671718a7a611d98dfad3a09
debhere <debmalya.mondal@outlook.com>
2025-11-10 07:55:29 +0530

    plusone

--- arrays/plusone.py
+++ arrays/plusone.py
@@ -4,8 +4,8 @@ from typing import List
 def plusOne(digits: List[int]) -> List[int]:
     """The function is a solution of a LeetCode problem
     where it adds 1 to the number represented by a given array and returns
-    the resulting number in an array. Eg:- If [1, 2, 3] is passed as a parameter
-    the function will add 1 to the number i.e., 124 and then return [1, 2, 4]
+    the resulting number in an array. Eg:- 123 is represented as an array [1, 2, 3] and is passed
+    as a parameter. The function will add 1 to the number i.e., 124 and then return [1, 2, 4] as an array.
 
     Args:
         digits (List[int]): An integer number represented in a list of integers.
