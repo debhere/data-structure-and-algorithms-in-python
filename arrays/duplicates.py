cf60826c54689b54066f29b34e8249f415006f63
debhere <debmalya.mondal@outlook.com>
2025-11-12 15:47:52 +0530

    docstring updated

--- arrays/duplicates.py
+++ arrays/duplicates.py
@@ -1,4 +1,15 @@
 def hasDuplicate(nums: list[int]) -> bool:
+    """This is LeetCode problem  to check if list has duplicate digits.
+
+    This function converts the list into a set and then compares the length of both the set and
+    the list. In case of dupes, the length of the set should be less.
+
+    Args:
+        nums(List[int]): list of integers
+
+    Returns:
+        bool: True if dupes exist otherwise False.
+    """
     return len(set(list(nums))) < len(nums)
 
 
