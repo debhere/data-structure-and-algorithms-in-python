4d9843455f003b77f37945d1c7f61d86a33c2eac
debhere <debmalya.mondal@outlook.com>
2025-11-12 15:43:53 +0530

    pascal triangle 2

--- arrays/pascal_traingle.py
+++ arrays/pascal_traingle.py
@@ -34,7 +34,35 @@ def pascalTriangle1(numRows: int) -> List[List[int]]:
     return result
 
 
+def pascalTriangle2(rowIndex: int) -> List[int]:
+    """This is another LeetCode problem with given an integer as rowIndex, the row of the Pascal's
+    triangle is returned.
+
+    Args:
+        rowIndex(int): row index.
+
+    Returns:
+        A list of integers.
+    """
+    triangle = [[1], [1, 1]]
+    idx = rowIndex
+    while idx >= 2:
+        l = list()
+        pre = triangle[-1]
+        for i in range(1, len(pre)):
+            ele = pre[i - 1] + pre[i]
+            l.append(ele)
+        l.append(1)
+        l.insert(0, 1)
+        triangle.append(l)
+        idx -= 1
+
+    return triangle[rowIndex]
+
+
 if __name__ == "__main__":
-    # print(pascalTriangle1(1))
     print(pascalTriangle1(3))
     print(pascalTriangle1(5))
+    print()
+    print(pascalTriangle2(3))
+    print(pascalTriangle2(5))
