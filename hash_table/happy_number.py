8a8183df5076e82f8c80743abf26c080a3b2e527
debhere <debmalya.mondal@outlook.com>
2025-11-25 07:54:12 +0530

    happy number

--- hash_table/happy_number.py
+++ hash_table/happy_number.py
@@ -2,6 +2,19 @@ from typing import List
 
 
 def isHappy(n: int) -> bool:
+    """This is a LeetCode problem, Given an integer number if the summation of the square of its digits
+    results into 1 then it is a happy number.
+
+    I am taking the digits and getting summation and storing the result into a dictionary. If I get the
+    summation as 1 then return as True else I continue the process until I encounter a result which is present
+    in the map denoting a repetition.
+
+    Args:
+        n (int): An integer.
+
+    Returns:
+        bool: Whether the argument is a happy number
+    """
     sq_map: dict[int] = {}
     while n >= 1:
         temp = 0
