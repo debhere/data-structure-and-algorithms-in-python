c5a6930021b2d5969346f5560d5be138769a2797
debhere <debmalya.mondal@outlook.com>
2026-01-08 07:54:54 +0530

    jewel and stones

--- string/jewels_stones.py
+++ string/jewels_stones.py
@@ -1,4 +1,17 @@
 def numJewelsInStones(jewels: str, stones: str) -> int:
+    """You're given strings jewels representing the types of stones that are jewels, and stones
+    representing the stones you have. Each character in stones is a type of stone you have. You want
+    to know how many of the stones you have are also jewels.
+
+    Letters are case-sensitive, so "a" is considered a different type of stone from "A".
+
+    Args:
+        jewels(str): stones that are jewels.
+        stones(str): stones.
+
+    Returns:
+        int: how many stones are jewels
+    """
     num = 0
     for letter in stones:
         if letter in jewels:
