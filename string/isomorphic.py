4bf730b1694e8b9795a6af497e5cc9963880c467
debhere <debmalya.mondal@outlook.com>
2026-02-26 10:30:01 +0530

    isomorphic string updated best solution

--- string/isomorphic.py
+++ string/isomorphic.py
@@ -20,9 +20,45 @@ def isIsomorphic(s: str, t: str) -> bool:
         return new_s == t
 
 
+def optimumSolution(s: str, t: str) -> bool:
+    """Given two strings s and t, determine if they are isomorphic. Two strings s and t are isomorphic
+    if the characters in s can be replaced to get t. All occurrences of a character must be replaced with
+    another character while preserving the order of characters. No two characters may map to the same character,
+    but a character may map to itself.
+
+    Args:
+        s: str, first string
+        t: str, second string
+
+    Returns:
+        bool: True if both the strings are isomorphic.
+    """
+    if s is None or t is None:
+        return False
+    if len(s) != len(t):
+        return False
+    map_st = {}
+    map_ts = {}
+
+    for e1, e2 in zip(s, t):
+        if e1 in map_st:
+            if map_st[e1] != e2:
+                return False
+        else:
+            if e2 in map_ts:
+                return False
+            map_st[e1] = e2
+            map_ts[e2] = e1
+
+    return True
+
+
 if __name__ == "__main__":
     print(isIsomorphic("egg", "add"))
     print(isIsomorphic("paper", "title"))
     print(isIsomorphic("foo", "bar"))
     print(isIsomorphic("bbbaaaba", "aaabbbba"))
     print(isIsomorphic("badc", "baba"))
+    print('\n')
+    print(optimumSolution("egg", "add"))
+    print(optimumSolution("badc", "baba"))
