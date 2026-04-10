f7bbf8178d772d01e013eb3ff68eb04b1a609228
debhere <debmalya.mondal@outlook.com>
2026-04-10 09:41:44 +0530

    max average of sub arrays

---
+++ sliding_window/max_average.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+def findMaxAverage(nums: List[int], k: int) -> float:
+    """You are given an integer array nums consisting of n elements, and an integer k. Finding a
+    contiguous subarray whose length is equal to k that has the maximum average value and return this
+    value.
+
+    Args:
+        nums: List[int], the input array of numbers.
+
+    Returns:
+        k: float, Highest average among the contagious sub-arrays.
+
+    """
+    max_sum = sum(nums[: k])
+    window = max_sum
+
+    for i in range(k, len(nums)):
+        window -= nums[i-k]
+        window += nums[i]
+
+        if window > max_sum:
+            max_sum = window
+
+    return max_sum / k
+
+
+if __name__ == "__main__":
+    print(findMaxAverage([1, 12, -5, -6, 50, 3], k=4))
